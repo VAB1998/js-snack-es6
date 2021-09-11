8d7a95620509b2ca4c079056a8ea5358b38f4892
VAB1998 <blanco.vincenzo@outlook.com>
2021-09-11 11:15:41 +0200

    Snack 1: Created a for loop to iterate through the array

---
+++ javascript/script.js
@@ -0,0 +1,36 @@
+/*
+    Creare un array di oggetti: ogni oggetto descriverà una bici 
+    da corsa con le seguenti proprietà: nome e peso.
+    Stampare a schermo la bici con peso minore.
+*/
+
+const bicycle = [
+    {
+        name : 'Blanchi',
+        weight : 5
+    },
+    {
+        name : 'Between',
+        weight : 6
+    },
+    {
+        name : 'Labc',
+        weight : 4
+    },
+    {
+        name : 'Labc',
+        weight : 12
+    },
+    {
+        name : 'Labc',
+        weight : 15
+    }
+]
+
+let lowerWeight = bicycle[0].weight;
+
+for(let i = 0; i < bicycle.length; i++){
+
+}
+
+console.log(lowerWeight);
